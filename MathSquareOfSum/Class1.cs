808e3f76807b612e8430684d110157e0067aab94
Takeshi Miyajima <rozyhead@gmail.com>
2020-06-28 13:17:53 +0900

    Initial commit

---
+++ MathSquareOfSum/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace MathSquareOfSum
+{
+    public class SimpleFormula : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<double> FirstNumber { get; set; }
+
+        [Category("Input")]
+        public InArgument<double> SecondNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<double> ResultNumber { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+            var result = System.Math.Pow(firstNumber + secondNumber, 2);
+            ResultNumber.Set(context, result);
+        }
+    }
+}
